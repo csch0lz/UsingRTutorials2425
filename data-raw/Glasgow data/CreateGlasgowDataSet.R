980357eda9a17b9615974dcc2356161a52075a1f
Wouter <w.denooy@uva.nl>
2020-12-01 09:26:17 +0100

    Session 3, initial commit.

--- data-raw/Glasgow data/CreateGlasgowDataSet.R
+++ data-raw/Glasgow data/CreateGlasgowDataSet.R
@@ -3,7 +3,7 @@
 
 library(tidyverse)
 
-# Create a tidy data frame.
+# Create a tidy data frame for Session 2. ####
 
 # Load and count friendship nominations.
 load("Glasgow-friendship.RData")
@@ -76,6 +76,10 @@ Glasgow <- cbind( #create matrix
       `1` = "1 none", `2` = "2 once or twice a year", `3` = "3 once a month", 
       `4` = "4 once a week", `5` = "5 more than once a week"
       ),
+    cannabis = recode(cannabis,
+                         `1` = "1 none", `2` = "2 tried once", `3` = "3 occasional", 
+                         `4` = "4 regular"
+    ),
     tobacco = recode(tobacco,
       `1` = "1 none", `2` = "2 occasional", `3` = "3 regular"
       ),
@@ -88,3 +92,91 @@ Glasgow <- cbind( #create matrix
 
 # save as .RData for inclusion in the package
 save(Glasgow, file = "Glasgow.RData")
+
+#cleanup
+rm(alcohol, cannabis, familysmoking, friendship.1, friendship.2, friendship.3, friendships, money, romantic, tobacco, age, sex.F, friend_dich, Glasgow)
+
+
+# Create an untidy data frame for Session 3. ####
+
+# Start with table of Session.
+# Load table.
+load("Glasgow.RData")
+
+# Create virtual neigbourhoods and add to student with distance to school category
+load("Glasgow-geographic.RData")
+# distance.1 is student by student matrix with distances at wave 1
+# replace NA by overall mean
+dist1 <- ifelse(is.na(distance.1), 
+           (ifelse(is.na(distance.2), 
+             (ifelse(is.na(distance.3), mean(distance.1, na.rm = TRUE), distance.3)), 
+              distance.2)), distance.1)
+clustering <- hclust(dist(dist1), method = "ward.D")
+burrough <- cutree(clustering, k=4) # cut tree into 4 clusters (one contains missings)
+# draw dendogram with red borders around the 5 clusters
+rect.hclust(clustering, k=4, border="red") 
+# add distance from school per group at wave 1 and group label to tidy frame
+GlasgowFriends <- as_tibble(cbind(burrough, dist.school), rownames = "student" ) %>%
+  select(student, burrough, school.dist = l1) %>%
+  group_by(burrough) %>%
+  mutate(schooldist = round(mean(school.dist, na.rm = TRUE), 2)) %>%
+  mutate(name = case_when(
+    burrough == 1 ~ "Burrough A",
+    burrough == 2 ~ "Burrough B",
+    burrough == 3 ~ "Burrough C",
+    TRUE ~ NA_character_,
+  ),
+    schooldist = ifelse(is.nan(schooldist), NA_real_, schooldist)
+  ) %>%
+  select(-school.dist) %>%
+  right_join(Glasgow, by = "student")
+#cleanup
+rm(angle.1, angle.2, angle.3, clustering, dist.school, dist1, distance.1, distance.2, distance.3, Glasgow, burrough)
+
+# Add nominated friends (student codes).
+# Load and count friendship nominations.
+load("Glasgow-friendship.RData")
+# Stack waves
+friendships <- rbind(friendship.1, friendship.2, friendship.3) %>%
+  #change into a data frame
+  as_tibble(rownames = "student") %>%
+  #add wave indicator
+  mutate(wave = c( rep("t1", 160), rep("t2", 160), rep("t3", 160))) %>%
+  # stack
+  pivot_longer(s001:s160, names_to = "nominated", values_to = "nomination" ) %>%
+  # drop nomination score 0 and 10
+  filter(nomination %in% c(1, 2))
+# add friendship nominations
+GlasgowFriends <- friendships %>%
+  # add rank number per student
+  group_by(wave, student) %>%
+  # best friends (coded 1) as first nomination(s)
+  arrange(nomination, .by_group = TRUE) %>%
+  mutate( constant = 1, nominnr = cumsum(constant)) %>%
+  # delete superfluous variables
+  select(-nomination, -constant) %>%
+  # spread
+  pivot_wider( names_from = nominnr, names_prefix = "friend_",  values_from = nominated) %>%
+  # add to base file
+  full_join(GlasgowFriends, by = c("student", "wave")) %>%
+  # reorder variables (and drop friendships)
+  select(student, burrough:romantic, wave:friend_6) %>%
+  ungroup()
+# add best friend (if any) with first and last wave
+GlasgowFriends <- friendships %>%
+  #select best friends
+  filter(nomination == 1) %>%
+  #get first and last wave of best friendship
+  group_by(student, nominated) %>%
+  summarise(from = min(wave), to = max(wave)) %>% 
+  #stack
+  pivot_longer(from:to, names_to = "bfperiod", values_to = "bfwave" ) %>%
+  # add to base file
+  full_join(GlasgowFriends, by = "student") %>%
+  # reorder and rename
+  select(student, burrough:friend_6, bestfriend = nominated, bfperiod, bfwave)
+#cleanup
+rm(friendship.1, friendship.2, friendship.3, friendships)
+
+# save as .RData for inclusion in the package
+save(GlasgowFriends, file = "GlasgowFriends.RData")
